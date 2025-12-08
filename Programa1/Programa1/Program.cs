597adf752a0fe844fb33627b12b231fb271013cc
MizunDev <jmperillap@gmail.com>
2025-12-08 12:08:38 -0500

    Update Program.cs

--- Programa1/Programa1/Program.cs
+++ Programa1/Programa1/Program.cs
@@ -15,6 +15,8 @@ namespace Programa1
 
             Console.WriteLine("El precio de la camisa es: {0} y su color es {1}", precioCamisa, colorCamisa);
             Console.WriteLine(precioCamisa);
+
+            Console.WriteLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consequat rutrum magna, rhoncus ornare turpis varius in. Donec quis placerat purus. Vestibulum tempor neque odio, a hendrerit dui pulvinar non. Pellentesque gravida, justo at euismod rutrum, justo mauris vestibulum felis, aliquam rhoncus erat dui quis ligula. Suspendisse fringilla orci purus, vel rutrum turpis tempor non. Duis imperdiet tortor et nisl vulputate, sit amet gravida diam consequat. Vivamus pharetra vitae felis in posuere. Duis et urna sit amet lacus aliquam fermentum nec sed risus. Donec non lacus velit. Ut molestie tellus sit amet nisi tempus, in hendrerit magna laoreet. Nam congue ut risus mattis luctus. Duis mauris lectus, tempor accumsan ipsum et, consequat facilisis elit.");
         }
     }
 }
