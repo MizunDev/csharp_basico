d90f0c8556550709f9fff05bb169044f4a602c22
MizunDev <jmperillap@gmail.com>
2025-12-08 11:17:40 -0500

    Inicializamos Repositorio de C# desde Cero

---
+++ MiNombre/MiNombre/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiNombre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jose Manuel");
+            Console.WriteLine("Buenos días");
+            Console.WriteLine("Hoy es un gran día para aprender C#");
+        }
+    }
+}
