01bf1937bd9de5026b3ff6462c91cf406e58f5bc
MizunDev <jmperillap@gmail.com>
2026-01-09 09:17:58 -0500

    seccion-5-metodos

---
+++ Seccion5/Seccion5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // WriteLine ReadLine Parse ToInt32
+        }
+
+        // [modificador] [tipo de retorno] [nombre del metodo]([parametros])
+        static void NombreDelMetodo()
+        {
+            // Cuerpo del metodo - instrucciones
+        }
+
+        /*
+         * 1. Metodos sin parametros ni tipo de retorno
+         * 2. Metodos con parametros
+         * 3. Metodos que devuelven un tipo de retorno
+         * 4. Metodos con parametros y que devuelven un tipo de retorno
+         */
+    }
+}
