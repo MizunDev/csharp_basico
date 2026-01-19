a6bb231a66c5f24da35013d943fbdacb602e8a2b
MizunDev <jmperillap@gmail.com>
2026-01-19 10:19:59 -0500

    Ejercicios seccion 6 arreglos

--- Seccion6/MatricesMultidimensionales/MatricesMultidimensionales/Program.cs
+++ Seccion6/MatricesMultidimensionales/MatricesMultidimensionales/Program.cs
@@ -10,15 +10,28 @@ namespace MatricesMultidimensionales
     {
         static void Main(string[] args)
         {
+            // Variables
+
+            int i; // Variable de control del ciclo externo
+            int j; // Variable de control del ciclo interno
             //tipo de dato[,] nombreMatriz = new tipo de dato[filas,columnas];
             double[,] ventas = new double[4, 3] { {100, 120, 205}, 
                                                   {115, 196, 300}, 
                                                   {157, 172, 245}, 
                                                   {130, 180, 281} };
 
-            // Declaracion e instancia de matriz de 3 dimensiones
-            int[,,] matriz3D = new int[2, 2, 3] { { { 1, 2, 3 }, { 4, 5, 6 } }, 
-                                                  { { 7, 8, 9 }, { 10, 11, 12 } } };
+            //ciclo para recorrer filas
+            for (i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Ventas de la tienda No. {0}", i + 1);
+                //ciclo para recorrer columnas
+                for (j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("  Mes {0}: {1}", j + 1, ventas[i, j]);
+                }
+            }
+
+
         }
     }
 }
