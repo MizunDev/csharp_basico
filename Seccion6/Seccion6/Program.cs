8ae96b7bec816d81fee8924ae93afe547c2a490c
MizunDev <jmperillap@gmail.com>
2026-01-21 10:36:02 -0500

    Final seccion 6

--- Seccion6/Seccion6/Program.cs
+++ Seccion6/Seccion6/Program.cs
@@ -10,26 +10,12 @@ namespace Seccion6
     {
         static void Main(string[] args)
         {
-            // Acceder a una matriz escalonada con length y GetLength
-            int i; // Cilco exterior
-            int j; // Ciclo interior
+            byte[] datos = new byte[5] { 1, 2, 3, 4, 5 };
 
-            double [][] matrizEscalonada = new double[4][]
-            {   new double[] {1.1, 2.2, 3.3},
-                new double[] {4.4, 5.5},
-                new double[] {6.6, 7.7, 8.8, 9.9},
-                new double[] {10, 215, 125}
-            };
-
-            for (i = 0; i < matrizEscalonada.Length; i++)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("Fila {0} tiene {1} columnas.", i, matrizEscalonada[i].Length);
-                for (j = 0; j < matrizEscalonada[i].Length; j++)
-                {
-                    Console.WriteLine("   Valor de la columna {0} es {1}", j, matrizEscalonada[i][j]);
-                }
+                Console.WriteLine(datos[i]);
             }
-
         }
     }
 }
